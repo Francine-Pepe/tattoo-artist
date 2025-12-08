cba150a04a3108c2c621a2b08522fb8ad707b9f3
Francine-Pepe <franmelopepe@gmail.com>
2025-12-08 17:39:24 +0100

    header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import NavItemProps from "./props/NavItemProps";
+import { navigationItem } from "../data";
+import Image from "./props/Image";
+import { logoData, headerBgData } from "../data";
+import MobileNav from "./MobileNav";
+import { NavLink } from "react-router-dom";
+
+function Navigation() {
+  return (
+    <header className="header-container">
+      <section className="header-logo">
+        <NavLink to="/">
+          <Image data={logoData} />
+        </NavLink>
+      </section>
+      <section className="header-nav">
+        <NavItemProps data={navigationItem} />
+      </section>
+      <section className="header-mobile-nav">
+        <MobileNav />
+      </section>
+      <section className="header-bg">
+        <Image data={headerBgData} />
+      </section>
+    </header>
+  );
+}
+
+export default Navigation;
