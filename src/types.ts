06267ed5bf0c2591f389d38da1c785ed1d56d953
Francine-Pepe <franmelopepe@gmail.com>
2026-01-05 15:36:09 +0100

    new data types

--- src/types.ts
+++ src/types.ts
@@ -15,24 +15,44 @@ export interface ImageItem {
   id?: number;
   alt: string;
   link?: string;
+  imageCarousel?: CarouselImage[];
 }
 
 export type ImageProps = {
   data: ImageItem[];
 };
 
+export interface CarouselImage {
+  parentId?: number;
+  src: string;
+  alt: string;
+  title?: string;
+}
+
+export interface CarouselProps {
+  images: CarouselImage[];
+  title?: string;
+  activeIndex?: number;
+  onImageChange?: (index: number) => void;
+}
+
+export interface CarouselState {
+  activeCardId?: number;
+  activeImageIndex: number;
+}
+
 export type IconProps = {
   name: string;
-}
+};
 
 export type TextItem = {
   id: number;
   heading?: string;
   subHeading?: string;
   text?: string;
-}
+};
 
 export type TextProps = {
   data: TextItem[];
   text?: TextItem[];
-}
+};
