ed83ec02f714a5768ebd8c04c3a9164dbc3253b2
Francine-Pepe <franmelopepe@gmail.com>
2026-01-05 15:35:51 +0100

    new local data

--- src/data.ts
+++ src/data.ts
@@ -5,6 +5,10 @@ import workImage from "./assets/images/tattoo-1.webp";
 import InstagramIcon from "./assets/icons/Instagram.svg";
 import LinkedinIcon from "./assets/icons/Linkedin.svg";
 import TwitterIcon from "./assets/icons/Twitter.svg";
+import Fauna from "./assets/images/tattoo-8.webp";
+import Flora from "./assets/images/tattoo-5.webp";
+import Abstract from "./assets/images/tattoo-12.webp";
+import Geometric from "./assets/images/tattoo-3.webp";
 
 export const navigationItem = [
   {
@@ -106,5 +110,86 @@ export const workTextData = [
   {
     id: 1,
     text: "Every design grows from a blend of natural forms and your own story, creating work that feels organic, intimate, and uniquely yours.",
+  },
+];
+
+export const workCardsData = [
+  {
+    id: 1,
+    image: Fauna,
+    alt: "Fauna tattoo type",
+    title: "Fauna",
+    imageCarousel: [
+      { parentId: 1, src: Fauna, alt: "Fauna tattoo type 1" },
+      { parentId: 1, src: Flora, alt: "Flora tattoo type 2" },
+      { parentId: 1, src: Abstract, alt: "Abstract tattoo type 3" },
+      { parentId: 1, src: Geometric, alt: "Geometric tattoo type 4" },
+    ],
+  },
+  {
+    id: 2,
+    image: Flora,
+    alt: "Flora tattoo type",
+    title: "Flora",
+    imageCarousel: [
+      { parentId: 2, src: Fauna, alt: "Fauna tattoo type 1" },
+      { parentId: 2, src: Flora, alt: "Flora tattoo type 2" },
+      { parentId: 2, src: Abstract, alt: "Abstract tattoo type 3" },
+      { parentId: 2, src: Geometric, alt: "Geometric tattoo type 4" },
+    ],
+  },
+  {
+    id: 3,
+    image: Abstract,
+    alt: "Abstract tattoo type",
+    title: "Abstract",
+    imageCarousel: [
+      { parentId: 3, src: Fauna, alt: "Fauna tattoo type 1" },
+      { parentId: 3, src: Flora, alt: "Flora tattoo type 2" },
+      { parentId: 3, src: Abstract, alt: "Abstract tattoo type 3" },
+      { parentId: 3, src: Geometric, alt: "Geometric tattoo type 4" },
+    ],
+  },
+  {
+    id: 4,
+    image: Geometric,
+    alt: "Geometric tattoo type",
+    title: "Geometric",
+    imageCarousel: [
+      { parentId: 4, src: Fauna, alt: "Fauna tattoo type 1" },
+      { parentId: 4, src: Flora, alt: "Flora tattoo type 2" },
+      { parentId: 4, src: Abstract, alt: "Abstract tattoo type 3" },
+      { parentId: 4, src: Geometric, alt: "Geometric tattoo type 4" },
+    ],
+  },
+];
+
+export const appointmentData = [
+  {
+    id: "referenceDesign",
+    label: "Reference design:",
+    name: "reference_image",
+    htmlFor: "referenceDesign",
+    type: "file",
+    accept: "image/*",
+    placeholder: "",
+  },
+  {
+    id: "tattooPlace",
+    label: "Tattoo place:",
+    name: "tattoo_place",
+    htmlFor: "tattooPlace",
+    type: "text",
+    accept: "",
+    placeholder: "e.g. Forearm",
+  },
+  {
+    id: "tattooSize",
+    label: "Tattoo size:",
+    name: "tattoo_size",
+    htmlFor: "tattooSize",
+    type: "text",
+    accept: "",
+    placeholder: "e.g. 10cm x 5cm",
   },
 ];
