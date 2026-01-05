83fed1c60fa380d7f51bee22ab2b3cec27d12f71
Francine-Pepe <franmelopepe@gmail.com>
2026-01-05 15:36:51 +0100

    text props component

--- src/components/props/Text.tsx
+++ src/components/props/Text.tsx
@@ -4,7 +4,7 @@ function Text({ data }: TextProps) {
   return (
     <section className="text-container">
       {data.map((item: TextItem) => (
-        <section key={item.id}>
+        <section key={item.id} className="text-content">
           <h1>{item.heading}</h1>
           <h2>{item.subHeading}</h2>
           <p>{item.text}</p>
