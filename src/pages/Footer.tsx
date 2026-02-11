a76b9821e2525bfcc8e33a4273e062f9fb5a028b
Francine-Pepe <franmelopepe@gmail.com>
2026-02-11 16:53:07 +0100

    footer component

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,14 +1,24 @@
 import IconsSvg from "../components/props/IconsSvg";
+import Image from "../components/props/Image";
 import Text from "../components/props/Text";
-import { footerTextData, socialMediaData } from "../data";
+import { footerTextData, socialMediaData, footerBgData } from "../data";
 
 function Footer() {
   return (
-    <footer className="footer-container">
-        <Text data={footerTextData} />
-        <IconsSvg data={socialMediaData} />
-    </footer>
-  )
+    <>
+      <footer className="footer-container">
+        <section className="footer-bg-image">
+          <Image data={footerBgData} />
+        </section>
+      </footer>
+      <footer className="footer-content-wrapper">
+        <section className="footer-content">
+          <Text data={footerTextData} />
+          <IconsSvg data={socialMediaData} />
+        </section>
+      </footer>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
