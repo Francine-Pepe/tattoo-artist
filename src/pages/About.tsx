55113943b865857b8984d0efd318debaa2d6fde2
Francine-Pepe <franmelopepe@gmail.com>
2026-02-11 16:52:53 +0100

    about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,17 @@
+import SimpleImage from "../components/props/SimpleImage";
+import SimpleText from "../components/props/SimpleText";
+import { aboutData } from "../data";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <main className="about-container">
+      <SimpleImage
+        image={aboutData.image}
+        alt={aboutData.alt}
+      />
+      <SimpleText text={aboutData.text} id={aboutData.id} />
+    </main>
+  );
 }
 
-export default About
+export default About;
